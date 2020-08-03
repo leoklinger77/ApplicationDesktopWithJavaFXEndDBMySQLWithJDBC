28a11e6409dc65734fcf044fe627498c26088d2b
Leandro Klinger <Leandro.klingeroliveira@gmail.com>
2020-08-02 23:36:41 -0300

    Saving a new Department

--- src/model/service/DepartmentService.java
+++ src/model/service/DepartmentService.java
@@ -2,17 +2,29 @@ package model.service;
 
 import java.util.List;
 
+import db.DbException;
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
 import model.entities.Department;
 
 public class DepartmentService {
-	
+
 	private DepartmentDao dao = DaoFactory.createDeparmentDao();
 
 	public List<Department> findAll() {
-		
+
 		return dao.findAll();
+	}
+
+	public void saveOrUpdate(Department obj) {
+		if(obj.getName() == null) {
+			throw new DbException("Inserir Department");
+		}
+		if (obj.getId() == null) {
+			dao.insert(obj);
+		} else {
+			dao.update(obj);
+		}
 	}
 
 }
